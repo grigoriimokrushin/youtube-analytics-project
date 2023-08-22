cd41c7c82db81e10470f05bfade72b3856a7e26e
Grigory Mokrushin <wedass@mail.ru>
2023-08-22 17:35:47 +0700

    Homework3

--- src/channel.py
+++ src/channel.py
@@ -18,9 +18,55 @@ class Channel:
         self.title = channel_info_dict['items'][0]['snippet']['title']
         self.description = channel_info_dict['items'][0]['snippet']['description']
         self.url = f"https://www.youtube.com/channel/{channel_id}"
-        self.view_count = channel_info_dict['items'][0]['statistics']['viewCount']
-        self.subscriber_count = channel_info_dict['items'][0]['statistics']['subscriberCount']
-        self.video_count = channel_info_dict['items'][0]['statistics']['videoCount']
+        self.view_count = int(channel_info_dict['items'][0]['statistics']['viewCount'])
+        self.subscriber_count = int(channel_info_dict['items'][0]['statistics']['subscriberCount'])
+        self.video_count = int(channel_info_dict['items'][0]['statistics']['videoCount'])
+
+    def __str__(self):
+        """Магический метод для пользователей."""
+        return f"{self.title} ({self.url})"
+
+    def __add__(self, other):
+        """
+        Метод срабатывает при сложении,
+        складываются количества подписчиков.
+        """
+        return self.subscriber_count + other.subscriber_count
+
+    def __sub__(self, other):
+        """
+        Метод срабатывает при вычитании,
+        вычитаются количества подписчиков.
+        """
+        return self.subscriber_count - other.subscriber_count
+
+    def __lt__(self, other):
+        """
+        Метод для сравнения "меньше"
+        по количеству подписчиков.
+        """
+        return self.subscriber_count < other.subscriber_count
+
+    def __le__(self, other):
+        """
+        Метод для сравнения "меньше или равно"
+        по количеству подписчиков.
+        """
+        return self.subscriber_count <= other.subscriber_count
+
+    def __gt__(self, other):
+        """
+        Метод для сравнения "больше"
+        по количеству подписчиков.
+        """
+        return self.subscriber_count > other.subscriber_count
+
+    def __ge__(self, other):
+        """
+        Метод для сравнения "больше или равно"
+        по количеству подписчиков.
+        """
+        return self.subscriber_count >= other.subscriber_count
 
     @property
     def channel_id(self):
--- tests/test_channel.py
+++ tests/test_channel.py
@@ -10,6 +10,12 @@ def moscowpython():
     return Channel('UC-OVMPlMA3-YCIeg4z5z23A')
 
 
+@pytest.fixture
+def highload():
+    """Создадим еще одну фикстуру с другим экземпляром класса."""
+    return Channel('UCwHL6WHUarjGfUM_586me8w')
+
+
 def test_channel_init(moscowpython):
     """Проверим как наш экземпляр класса инициализироваля."""
     assert moscowpython.channel_id == 'UC-OVMPlMA3-YCIeg4z5z23A'
@@ -27,6 +33,27 @@ def test_get_service(moscowpython):
     """Проверим метод класса get_service, выдает ли он нам нужный объект."""
     assert moscowpython.get_service() == Channel.youtube
 
+
 def test_to_json(moscowpython):
     """Проверит метод to_json, создал ли он фаил и правильны ли путь до него выдает."""
     assert moscowpython.to_json('python.json') == os.path.abspath('python.json')
+
+
+def test_str(moscowpython):
+    """Проверяем наш магический метод."""
+    assert str(moscowpython) == 'MoscowPython (https://www.youtube.com/channel/UC-OVMPlMA3-YCIeg4z5z23A)'
+
+
+def test_add_and_sub(moscowpython, highload):
+    """Проверяем как складываются и вычитаются наши экземпляры класса."""
+    assert moscowpython + highload == 102600
+    assert moscowpython - highload == -50000
+
+
+def test_le_lt_ge_gt(moscowpython, highload):
+    """Проверяем как сравниваются наши экземпляры класса."""
+    assert (moscowpython < highload) == True
+    assert (moscowpython <= highload) == True
+    assert (moscowpython > highload) == False
+    assert (moscowpython >= highload) == False
+    assert (moscowpython == highload) == False
